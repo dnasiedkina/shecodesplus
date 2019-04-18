c4a04cead360c9769655555c5ad2eaa50ea49379
dnasiedkina <48137068+dnasiedkina@users.noreply.github.com>
2019-04-18 20:20:04 +0100

    final project js and css

---
+++ app.js
@@ -0,0 +1,5 @@
+  axios.get('/user?ID=20e63c03dedacdbe6b617395a08d5403')
+      .then(function (response) {
+        // handle success
+        console.log(response);
+  api.openweathermap.org / data / 2.5 / weather ? q = { London }
